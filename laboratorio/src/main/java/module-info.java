f3c34d4700ebac466e11dc4b9470ce47a75ceb0d
Andre_Valane <andremvalane@gmail.com>
2026-05-04 10:00:28 -0300

    Organização das pastas do projeto

--- laboratorio/src/main/java/module-info.java
+++ laboratorio/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module javafx.laboratorio {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens javafx.laboratorio to javafx.fxml;
     exports javafx.laboratorio;
