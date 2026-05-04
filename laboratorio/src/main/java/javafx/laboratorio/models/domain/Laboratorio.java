f784c1ed15902f3279598c10bc17d78278cc8fc1
Andre_Valane <andremvalane@gmail.com>
2026-05-04 09:52:07 -0300

    Organização das pastas do projeto

---
+++ laboratorio/src/main/java/javafx/laboratorio/models/domain/Laboratorio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafx.laboratorio.models.domain;
+
+/**
+ *
+ * @author 20241si041
+ */
+public class Laboratorio {
+    
+}
